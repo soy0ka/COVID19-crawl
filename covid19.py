92871780bde1751a1b52e017b205b92feaa0a227
Muzihuzi <55011525+Muzihuzi@users.noreply.github.com>
2020-02-23 14:33:02 +0900

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -6,15 +6,8 @@ html = request.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-# <a> 태그를 포함하는 요소를 추출합니다.
-
 links = soup.select('li > a')
-
-# 모든 링크에 하나씩 접근합니다.
-
 for link in links:
-
-    # 링크가 href 속성을 가지고 있다면
 
     if link.has_attr('href'):
         
