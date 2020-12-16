c4f8bd2b30065a6ac74e8e8f542846d51fea4374
Tim232 <junseongi284@gmail.com>
2020-12-16 10:22:40 +0900

    비동기

--- covid19.py
+++ covid19.py
@@ -1,15 +1,13 @@
-import requests
+import requests_async as requests
 from bs4 import BeautifulSoup
 
-request = requests.get('http://ncov.mohw.go.kr/index_main.jsp')
-html = request.text
+async def main():
+    request = await requests.get('http://ncov.mohw.go.kr/index_main.jsp')
+    html = request.text
 
-soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
-links = soup.select('li > a')
-for link in links:
-
-    if link.has_attr('href'):
-        
-            print(link.text)
+    links = soup.select('li > a')
+    for link in [link for link in linkes if link.has_attr('href')]:
+        print(link.text)
 
